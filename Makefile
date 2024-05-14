1d70ca312b2891627a3052050554af028494d4b9
Joey Ekstrom <joey@ekstech.net>
2024-05-14 12:12:36 -0700

    Neovim setup

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@
 .PHONY: update
 update:
 	home-manager switch --flake .#myprofile
+
+clean:
+	nix-collect-garbage -d
--- home.nix
+++ home.nix
@@ -2,7 +2,20 @@
 {
   home = {
     packages = with pkgs; [
-      hello
+      bat
+      bitwarden-cli
+      htop
+      awscli2
+      chromium
+      fq
+      gnumake
+      inetutils
+      jq
+      pstree
+      python3
+      silver-searcher
+      tmux
+      wget
     ];
 
     username = "jce";
@@ -11,4 +24,29 @@
     # Don't ever change this after the first build.
     stateVersion = "23.11";
   };
+
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+
+    git.enable = true;
+      
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
+      plugins = with pkgs.vimPlugins; [
+        vim-fugitive
+        vim-nix
+        awesome-vim-colorschemes
+        ack-vim
+      ];
+
+      extraConfig = lib.fileContents ./init.vim;
+    };
+  };
+
 }
