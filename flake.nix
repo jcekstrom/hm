45d07a511d6998b2f79fd86b48dc79773ab4cb0b
Joey Ekstrom <joey@ekstech.net>
2025-08-08 17:04:46 -0700

    Really removing ghostty

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:/nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
@@ -22,9 +22,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 		
-		ghostty = {
-			url = "github:ghostty-org/ghostty";
-		};
+		#ghostty = {
+		#	url = "github:ghostty-org/ghostty";
+		#};
   };
 
   outputs = inputs@{ nixpkgs, nixpkgs-unstable, home-manager, nixvim-config, ... }:
